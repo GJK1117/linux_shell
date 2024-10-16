3f3cc94ce72955c1daac63d0c5a1d2da61c07b15
김준곤 <118426175+GJK1117@users.noreply.github.com>
2024-10-16 14:27:41 +0900

    Update NFS_server_shell.sh

--- setup_project/NFS_server_shell.sh
+++ setup_project/NFS_server_shell.sh
@@ -58,9 +58,13 @@ EXPORTS_FILE="/etc/exports"
 echo "$NFS_DIR 192.168.138.0/24(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a $EXPORTS_FILE
 
 # 사용자별 디렉터리 공유 설정 추가
-echo "/autofs/user01  *(rw,sync,sec=sys,no_root_squash)" | sudo tee -a $EXPORTS_FILE
-echo "/autofs/user02  *(rw,sync,sec=sys,no_root_squash)" | sudo tee -a $EXPORTS_FILE
-echo "/autofs/user03  *(rw,sync,sec=sys,no_root_squash)" | sudo tee -a $EXPORTS_FILE
+# echo "/autofs/user01  *(rw,sync,sec=sys,no_root_squash)" | sudo tee -a $EXPORTS_FILE
+# echo "/autofs/user02  *(rw,sync,sec=sys,no_root_squash)" | sudo tee -a $EXPORTS_FILE
+# echo "/autofs/user03  *(rw,sync,sec=sys,no_root_squash)" | sudo tee -a $EXPORTS_FILE
+
+echo "/autofs/user01  192.168.138.0/24(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a $EXPORTS_FILE
+echo "/autofs/user02  192.168.138.0/24(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a $EXPORTS_FILE
+echo "/autofs/user03  192.168.138.0/24(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a $EXPORTS_FILE
 
 sudo exportfs -r
 echo "NFS exports 적용 완료."
