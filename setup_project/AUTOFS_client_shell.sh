944cb5228462d9b466148ec255dc9358bb6a71d6
김준곤 <118426175+GJK1117@users.noreply.github.com>
2024-10-17 19:59:03 +0900

    Update AUTOFS_client_shell.sh

--- setup_project/AUTOFS_client_shell.sh
+++ setup_project/AUTOFS_client_shell.sh
@@ -12,9 +12,9 @@ EOL
 
 # 사용자 홈 디렉터리 NFS 마운트를 위한 Indirect Map 설정
 sudo cat <<EOL > /etc/auto.home
-user01 -rw,sync,sec=sys $SERVER_DS:/autofs/user01
-user02 -rw,sync,sec=sys $SERVER_DS:/autofs/user02
-user03 -rw,sync,sec=sys $SERVER_DS:/autofs/user03
+user01 -rw,sync,sec=sys $SERVER_DS:/home/user01
+user02 -rw,sync,sec=sys $SERVER_DS:/home/user02
+user03 -rw,sync,sec=sys $SERVER_DS:/home/user03
 EOL
 
 # AutoFS 서비스 재시작 및 활성화
